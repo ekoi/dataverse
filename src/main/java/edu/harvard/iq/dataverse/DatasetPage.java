6fa5f4028c9a47ec6de48f5477d26c5b14ee80f1
ekoi <eko.indarto@dans.knaw.nl>
2020-08-22 06:05:38 +0200

    Add Autocomplete for CVM.

--- src/main/java/edu/harvard/iq/dataverse/DatasetPage.java
+++ src/main/java/edu/harvard/iq/dataverse/DatasetPage.java
@@ -55,10 +55,8 @@ import static edu.harvard.iq.dataverse.util.StringUtil.isEmpty;
 
 import edu.harvard.iq.dataverse.util.StringUtil;
 import edu.harvard.iq.dataverse.util.SystemConfig;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+
+import java.io.*;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -83,6 +81,7 @@ import javax.inject.Named;
 
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.file.UploadedFile;
+import javax.json.*;
 import javax.validation.ConstraintViolation;
 import org.apache.commons.httpclient.HttpClient;
 //import org.primefaces.context.RequestContext;
@@ -3512,7 +3511,60 @@ public class DatasetPage implements java.io.Serializable {
         }
 
     }
-     
+
+    public class CVM {
+        String sourceName;
+        String sourceUrl;
+        List<String> vocabs;
+        List<String> keys;
+        public CVM(String sourceName, String sourceUrl, List<String> vocabs, List<String> keys){
+            this.sourceName = sourceName;
+            this.sourceUrl = sourceUrl;
+            this.vocabs = vocabs;
+            this.keys = keys;
+        }
+        public String getSourceName() {
+            return sourceName;
+        }
+        public String getSourceUrl() {
+            return sourceUrl;
+        }
+        public List<String> getVocabs() {
+            return vocabs;
+        }
+        public List<String> getKeys() {
+            return keys;
+        }
+    }
+    public Map<String, CVM> getCVMConf(){
+        Map <String, CVM> cvmMap = new HashMap<>();
+        String cvmSetting = settingsService.getValueForKey(SettingsServiceBean.Key.CVMConf);
+        if (cvmSetting == null || cvmSetting.isEmpty())
+            return cvmMap;
+
+        JsonReader jsonReader = Json.createReader(new StringReader(settingsService.getValueForKey(SettingsServiceBean.Key.CVMConf)));
+        JsonArray cvmConfJsonArray = jsonReader.readArray();
+        jsonReader.close();
+        if (cvmConfJsonArray != null) {
+            for (JsonObject jo : cvmConfJsonArray.getValuesAs(JsonObject.class)) {
+                JsonArray v = jo.getJsonArray("vocabs");
+                List<String> vs = new ArrayList<>();
+                for (JsonString elm: v.getValuesAs(JsonString.class)){
+                    vs.add(elm.getString());
+                }
+                JsonArray k = jo.getJsonArray("keys");
+                List<String> ks = new ArrayList<>();
+                for (JsonString elm: k.getValuesAs(JsonString.class)){
+                    ks.add(elm.getString());
+                }
+                CVM CVm = new CVM(jo.getString("source-name"), jo.getString("source-url"), vs, ks);
+                cvmMap.put(jo.getString("aci"), CVm);
+
+            }
+        }
+        return cvmMap;
+    }
+
     public String save() {
         //Before dataset saved, write cached prov freeform to version
         if (systemConfig.isProvCollectionEnabled()) {
--- src/main/java/edu/harvard/iq/dataverse/settings/SettingsServiceBean.java
+++ src/main/java/edu/harvard/iq/dataverse/settings/SettingsServiceBean.java
@@ -193,6 +193,11 @@ public class SettingsServiceBean {
         GuidesBaseUrl,
 
         /**
+         * Controlled Vocabulary URL
+         */
+        CVMConf,
+
+        /**
          * A link to an installation of https://github.com/IQSS/miniverse or
          * some other metrics app.
          */
